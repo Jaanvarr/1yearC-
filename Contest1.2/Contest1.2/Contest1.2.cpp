ff29e5d1154929d9a39a6f89c6bffe6c6d8665a2
unknown <artur.kolotkin@gmail.com>
2025-05-13 16:02:44 +0300

    add labs

---
+++ Contest1.2/Contest1.2/Contest1.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+
+
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    if (n > 0) {
+        (n % 2) ? n = ((n + 1) >> 1) * n : n = (n >> 1) * (n + 1);     //Overflow protection by bit-shifting
+    }
+    else if (n == 0) {
+        n = 1;
+    }
+    else {
+        n = -n;
+        (n % 2) ? n = ((n + 1) >> 1) * n : n = (n >> 1) * (n + 1);
+        n = 1 - n;
+    }
+    std::cout << n;
+    return 0;
+}
